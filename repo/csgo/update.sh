95d4cbaf411db509bc9e54e437f175b5eced73ff
Mark Johansen <mark@johansen.wtf>
2019-04-23 21:04:13 +0000

    initial setup

---
+++ repo/csgo/update.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/home/steam/Steam/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 validate +quit
---
+++ repo/steamcmd/install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sudo apt-get install lib32gcc1 -y
+
+curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
