d5ecef31ae85fe98ffff25e6855e35292d051ca9
Mark Johansen <mark@johansen.wtf>
2019-04-23 21:11:15 +0000

    made a little mistake

--- repo/steamcmd/install.sh
+++ repo/steamcmd/install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get install lib32gcc1 -y
+sudo apt-get install lib32gcc1 screen -y
 
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
