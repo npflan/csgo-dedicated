8413ef9e9d5e494f6b1f5105b3b9ae7df60b27d0
Mark Johansen <mark@johansen.wtf>
2019-04-23 21:08:37 +0000

    few changes to correct pathing

--- repo/csgo/update.sh
+++ repo/csgo/update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/home/steam/Steam/steamcmd.sh +login anonymous +force_install_dir /home/steam/csgo +app_update 740 validate +quit
+/srv/repo/steamcmd/steamcmd.sh +login anonymous +force_install_dir /srv/repo/csgo +app_update 740 validate +quit
