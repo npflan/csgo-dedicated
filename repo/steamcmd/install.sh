88b9417ff743da77ca15eab1c44acf50153ce700
Mark Johansen <mark@johansen.wtf>
2019-04-23 21:24:33 +0000

    Added configs

--- repo/steamcmd/install.sh
+++ repo/steamcmd/install.sh
@@ -2,4 +2,10 @@
 
 sudo apt-get install lib32gcc1 screen -y
 
+pushd /srv/repo/steamcmd
+
 curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
+
+/srv/repo/steamcmd/steamcmd.sh +login anonymous +quit
+
+popd
